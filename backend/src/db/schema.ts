9fbd437b538802abff8a06bf39226e09c37b06f8
nakatsu <k-nakatsu@digeon.co>
2024-03-17 14:35:53 +0000

    add drizzle

---
+++ backend/src/db/schema.ts
@@ -0,0 +1,13 @@
+import { sql } from "drizzle-orm";
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const ArticleSchema = sqliteTable("articles", {
+	id: integer("id", { mode: "number" })
+		.primaryKey({ autoIncrement: true })
+		.notNull(),
+	title: text("title").notNull(),
+	content: text("content").notNull(),
+	createdAt: integer("created_at", { mode: "timestamp" })
+		.default(sql`CURRENT_TIMESTAMP`)
+		.notNull(),
+});
