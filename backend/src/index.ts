d6834cebfc2c90e226dd3ca0aad1b8789318a509
nakatsu <k-nakatsu@digeon.co>
2024-03-17 12:26:50 +0000

    add biome

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,13 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 
-const app = new Hono();
+type Env = {
+	Bindings: {
+		DB: D1Database;
+	};
+};
+
+const app = new Hono<Env>();
 
 app.use("*", cors());
 
