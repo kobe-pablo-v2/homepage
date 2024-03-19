a23cae30dac0c4eaabf0aaec57ffb6a5c588d5a0
nakatsu <k-nakatsu@digeon.co>
2024-03-19 02:17:58 +0000

    fix table

--- backend/drizzle/0000_yummy_slayback.sql
+++ backend/drizzle/0000_yummy_slayback.sql
@@ -2,5 +2,6 @@ CREATE TABLE `articles` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`title` text NOT NULL,
 	`content` text NOT NULL,
+	`image_url` text NOT NULL,
 	`created_at` integer DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
